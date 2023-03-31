87e98c429068383489929a33b008561d7cfa7427
JaeYoungYoon <jy345678@naver.com>
2023-03-31 16:46:01 +0900

    board list, command, dao 추가

---
+++ src/servlet_project/jy/command/BCommand.java
@@ -0,0 +1,17 @@
+package servlet_project.jy.command;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardController
+ */
+
+public abstract interface BCommand {
+	void execute(HttpServletRequest request, HttpServletResponse response);
+}
+
